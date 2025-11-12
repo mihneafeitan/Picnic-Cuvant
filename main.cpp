44dfe62733712d4d009c99f823280a92265aae7f
mihneafeitan <mihnea.hristache@gmail.com>
2025-11-12 10:05:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "include/Joc.h"
 #include <iostream>
-#include <stdexcept> // Pentru std::exception
+#include <stdexcept>
 
 int main() {
     try {
-        // Pornim jocul folosind fisierul CRIPTAT
-        // CMake va copia 'nivele.dat' in folderul 'bin/'
         Joc picnicCuvant("nivele.dat");
         picnicCuvant.ruleaza();
 
@@ -13,10 +11,5 @@ int main() {
         std::cerr << "O eroare critica a oprit jocul: " << e.what() << std::endl;
         return 1;
     }
-
-    // Demonstratia pentru Regula celor Trei (daca e ceruta)
-    // o poti adauga aici, in afara try-catch-ului principal
-    // sau chiar inainte de `picnicCuvant.ruleaza();`
-
     return 0;
 }
